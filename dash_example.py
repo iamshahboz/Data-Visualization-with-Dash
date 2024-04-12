90f3f1cf6e10ece735ad2267a20573114ec04e60
Shahboz <sharafzodashahboz@gmail.com>
2024-04-12 12:01:33 +0500

    app example

--- dash_example.py
+++ dash_example.py
@@ -1,27 +1,25 @@
-from dash import Dash, html, dcc
-from dash.dependencies import Input, Output
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px 
+import pandas as pd 
 
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-app = Dash()
+app = Dash(__name__)
 
-app.layout = html.Div(children=[html.H1('Dash tutorial'),
-                    dcc.Input(id='input', 
-                              value='Enter something', type='text'),
-                    html.Div(id='output')
-                    ])
+app.layout = html.Div([
+    html.H1(children='Title of Dash app', style={'textAlign': 'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
 
-@app.callback(
-    Output(component_id='output', component_property='children'),
-    [Input(component_id='input', component_property='value')]
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
 )
-
-def update_value(input_data):
-    return "Input: {}".format(input_data)
-
-
+def update_graph(value):
+    dff = df[df.country == value]
+    return px.line(dff, x= 'year', y='pop')
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-
-
-
+    app.run(debug=True)
+    
